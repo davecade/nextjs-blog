1aaa00d23d6b5ca7a61fc1097bd29b337f58abf9
Dave <dave.cadelina@outlook.com>
2023-03-10 00:42:13 +1100

    styles

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 const FirstPost = () => {
   return (
-    <>
-        <h1>first-post</h1>
-        <h2>
-            <Link href="/">Back to home</Link>
-        </h2>
-    </>
+    <Layout>
+      <Head>
+        <title>first-post</title>
+      </Head>
+      <h2>
+          <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
   )
 }
+
+// You are up to Assets, Metadata, and CSS -- Page 8
 
 export default FirstPost
